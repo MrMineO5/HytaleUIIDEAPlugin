0e9f0364485051d56d2b4cdcc3a5831625c3208e
UltraDev <petzmagnus@gmail.com>
2026-02-19 22:52:24 +0100

    Add ui file icon

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ version = "1.1.1"
 
 repositories {
     mavenCentral()
-//    maven("https://mvn.ultradev.app/snapshots")
-    mavenLocal()
+    maven("https://mvn.ultradev.app/snapshots")
+//    mavenLocal()
     intellijPlatform {
         defaultRepositories()
     }
@@ -19,7 +19,7 @@ repositories {
 // Configure IntelliJ Platform Gradle Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
-    api("app.ultradev:HytaleUIParser:2.1.0")
+    api("app.ultradev.hytaleui:all:3.0.0")
     
     intellijPlatform {
         intellijIdea("2024.3.2")
--- src/main/kotlin/app/ultradev/uifiles/UIFileType.kt
+++ src/main/kotlin/app/ultradev/uifiles/UIFileType.kt
@@ -1,13 +1,14 @@
 package app.ultradev.uifiles
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 class UIFileType : LanguageFileType(UILanguage.INSTANCE) {
     override fun getName(): String = "UI File"
     override fun getDescription(): String = "Hytale UI file"
     override fun getDefaultExtension(): String = "ui"
-    override fun getIcon(): Icon? = null
+    override fun getIcon(): Icon = IconLoader.getIcon("/META-INF/uifile.svg", javaClass)
 
     companion object {
         val INSTANCE = UIFileType()
